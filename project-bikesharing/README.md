62528709db822ea8723736d80b7cec5e3f64b4be
musajoshua <gidijosh@gmail.com>
2021-06-06 10:20:14 +0100

    bike sharing project solution

---
+++ project-bikesharing/README.md
@@ -0,0 +1,85 @@
+# Predicting Bike-Sharing Patterns
+
+Neural Network developed for predicting daily bike rental ridership.
+
+## Getting Started
+
+This Bike-Sharing-Dataset has the number of riders for each hour of each day from January 1 2011 to December 31 2012. The number of riders is split between casual and registered, summed up in the cnt column. You can see the first few rows of the data above.
+
+Below is a plot showing the number of bike riders over the first 10 days or so in the data set. (Some days don't have exactly 24 entries in the data set, so it's not exactly 10 days.) You can see the hourly rentals here. This data is pretty complicated! The weekends have lower over all ridership and there are spikes when people are biking to and from work during the week. Looking at the data above, we also have information about temperature, humidity, and windspeed, all of these likely affecting the number of riders. You'll be trying to capture all this with your model.
+
+![dataset](./assets/no_of_bikers.png)
+
+### Prerequisites
+
+Thinks you have to install or installed on your working machine:
+
+* Python 3.7
+* Numpy (win-64 v1.15.4)
+* Pandas (win-64 v0.23.4)
+* Matplotlib (win-64 v3.0.2)
+* Jupyter Notebook
+* Torchvision (win-64 v0.2.1)
+* PyTorch (win-64 v0.4.1)
+
+### Environment:
+* [Miniconda](https://conda.io/miniconda.html) or [Anaconda](https://www.anaconda.com/download/)
+
+### Installing
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) or
+[miniconda](https://conda.io/miniconda.html) or [Anaconda](https://www.anaconda.com/download/) to install your packages.  
+A step by step guide to install the all necessary components in Anaconda for a Windows-64 System:
+```bash
+conda install -c conda-forge numpy
+conda install -c conda-forge pandas
+conda install -c conda-forge matplotlib
+pip install torchvision
+conda install -c pytorch pytorch
+```
+
+## Jupyter Notebook
+* `Predicting_bike_sharing_data.ipynb`
+
+This jupyter notebook describe the whole project from udacity, from the beginning to the end.
+
+## Running the project
+
+The whole project is located in the jupyter notebook file `Predicting_bike_sharing_data.ipynb` and it's include the training an the prediction part. The neural network is implemented in the file `my_answer.py` and used from the jupyter notebook.
+
+### Parameters of training
+
+To change to interations, the amount of hidden notes and some other parameters for the neural network, you can adapt these global constants inside the python file `my_answer.py` and watch the results.
+
+```python
+#########################################################
+# Set your hyperparameters here
+##########################################################
+iterations      = 4000      # 4000 / 10000 / 100000 / 200000
+learning_rate   = 1         # 1     
+hidden_nodes    = 10        # 10
+output_nodes    = 1         # 1
+```
+I got my best results with these values of parameters.
+
+## Checkout the training results
+
+```bash
+Progress: 100.0% ... Training loss: 0.067 ... Validation loss: 0.137
+```
+See the visual results over iteration:
+
+![training results](./assets/training_vs_validation.png)
+
+## Checkout the prediction results
+
+I use the test data to show how well the neural network is modeling the data.
+
+![training results](./assets/test_output_vs_label.png)
+
+## Authors
+
+* Musa Joshua
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
--- project-bikesharing/my_answers.py
+++ project-bikesharing/my_answers.py
@@ -69,7 +69,10 @@ class NeuralNetwork(object):
 
         # TODO: Output layer - Replace these values with your calculations.
         final_inputs = np.dot(hidden_outputs, self.weights_hidden_to_output) # signals into final output layer
-        final_outputs = self.activation_function(final_inputs) # signals from final output layer
+        
+        # It is stated above that the output of the final node is the same as the input 
+        # since it has an activation function f(x) = x
+        final_outputs = final_inputs # signals from final output layer
         
         return final_outputs, hidden_outputs
 
@@ -90,17 +93,22 @@ class NeuralNetwork(object):
         # TODO: Output error - Replace this value with your calculations.
         error = y - final_outputs # Output layer error is the difference between desired target and actual output.
         
-        # TODO: Backpropagated error terms - Replace these values with your calculations.
-        output_error_term = error * final_outputs * (1- final_outputs)
-        
         # TODO: Calculate the hidden layer's contribution to the error
-        hidden_error = self.weights_hidden_to_output * output_error_term
-        h = np.dot(X, self.weights_input_to_hidden)
-        hidden_error_term = hidden_error * self.activation_function(h) * ( 1- self.activation_function(h))
+        # hidden error = weights_h_o * output_error_term, but since output_error_term == error (reason stated below)
+        # or another means could be to rearrange the inorder so that output_error_term is defined before hidden_error
+        # so that the value can be referenced in this equation
+        hidden_error = np.dot(self.weights_hidden_to_output, error)
+        
+        # TODO: Backpropagated error terms - Replace these values with your calculations.
+        # output error term = error * f'(x), but since f(x) = x, f'(x) = 1
+        output_error_term = error * 1
+        # hidden error term = hidden_error * f'(h), but since f'(h) = f(h) * (1 - f(h))
+        hidden_error_term = hidden_error * hidden_outputs * (1 - hidden_outputs)
+        
         # Weight step (input to hidden)
         delta_weights_i_h += hidden_error_term * X[:, None]
         # Weight step (hidden to output)
-        delta_weights_h_o += output_error_term * hidden_error
+        delta_weights_h_o += output_error_term * hidden_outputs[:, None]
         return delta_weights_i_h, delta_weights_h_o
 
     def update_weights(self, delta_weights_i_h, delta_weights_h_o, n_records):
@@ -114,7 +122,6 @@ class NeuralNetwork(object):
 
         '''
         self.weights_hidden_to_output += self.lr * delta_weights_h_o / n_records
-        print('weights_h_o:'+'\n'+str(self.weights_hidden_to_output ))
         # update hidden-to-output weights with gradient descent step
         self.weights_input_to_hidden += self.lr * delta_weights_i_h / n_records 
         # update input-to-hidden weights with gradient descent step
@@ -134,7 +141,10 @@ class NeuralNetwork(object):
         
         # TODO: Output layer - Replace these values with the appropriate calculations.
         final_inputs = np.dot(hidden_outputs, self.weights_hidden_to_output) # signals into final output layer
-        final_outputs = self.activation_function(final_inputs) # signals from final output layer 
+        
+        # It is stated above that the output of the final node is the same as the input 
+        # since it has an activation function f(x) = x
+        final_outputs = final_inputs # signals from final output layer
         
         return final_outputs
 
@@ -142,7 +152,7 @@ class NeuralNetwork(object):
 #########################################################
 # Set your hyperparameters here
 ##########################################################
-iterations = 100
-learning_rate = 0.1
-hidden_nodes = 5
+iterations = 4000
+learning_rate = 1
+hidden_nodes = 10
 output_nodes = 1
